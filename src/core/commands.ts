ce891e88b6608b96cd22ac9d4673297021974043
Jeremy Bahadirli <jeremybahadirli@gmail.com>
2026-08-02 16:55:04 -0500

    Refactor aid to flid

--- src/core/commands.ts
+++ src/core/commands.ts
@@ -32,7 +32,7 @@ export function executeCommand(rawCommand: string): CommandResult {
 	return { accept: false, message: `${rawCommand}` };
 }
 
-function executeDatablockSlotCommand(slot: number, aid: string): CommandResult {
+function executeDatablockSlotCommand(slot: number, flid: string): CommandResult {
 	if (!isDatablockSlot(slot)) {
 		return {
 			accept: false,
@@ -40,8 +40,8 @@ function executeDatablockSlotCommand(slot: number, aid: string): CommandResult {
 		};
 	}
 
-	const aircraft = findAircraftByAid(aid);
-	if (!aircraft) return unknownAid(aid);
+	const aircraft = findAircraftByFlid(flid);
+	if (!aircraft) return unknownFlid(flid);
 
 	aircraft.display.datablockSlot = slot;
 	return {
@@ -50,9 +50,9 @@ function executeDatablockSlotCommand(slot: number, aid: string): CommandResult {
 	};
 }
 
-function executeHaloCommand(aid: string, radiusNm: 3 | 5): CommandResult {
-	const aircraft = findAircraftByAid(aid);
-	if (!aircraft) return unknownAid(aid);
+function executeHaloCommand(flid: string, radiusNm: 3 | 5): CommandResult {
+	const aircraft = findAircraftByFlid(flid);
+	if (!aircraft) return unknownFlid(flid);
 
 	if (aircraft.display.halo && aircraft.display.haloRadiusNm === radiusNm) {
 		aircraft.display.halo = false;
@@ -66,12 +66,12 @@ function executeHaloCommand(aid: string, radiusNm: 3 | 5): CommandResult {
 	};
 }
 
-function findAircraftByAid(aid: string): Aircraft | undefined {
-	const normalizedAid = aid.trim().toUpperCase();
+function findAircraftByFlid(flid: string): Aircraft | undefined {
+	const normalizedFlid = flid.trim().toUpperCase();
 	return simState.aircraftList.find(
 		(aircraft) =>
-			aircraft.cid === normalizedAid ||
-			aircraft.callsign === normalizedAid,
+			aircraft.cid === normalizedFlid ||
+			aircraft.callsign === normalizedFlid,
 	);
 }
 
@@ -79,9 +79,9 @@ function isDatablockSlot(value: number): value is DatablockSlot {
 	return VALID_DATABLOCK_SLOTS.includes(value as DatablockSlot);
 }
 
-function unknownAid(aid: string): CommandResult {
+function unknownFlid(flid: string): CommandResult {
 	return {
 		accept: false,
-		message: `UNKNOWN AID ${aid.toUpperCase()}`,
+		message: `UNKNOWN FLID ${flid.toUpperCase()}`,
 	};
 }
